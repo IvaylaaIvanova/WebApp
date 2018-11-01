8917b6e9203a6adeebf3695bebb462f83bcffb3a
eleonora98 <eleonora_peneva@abv.bg>
2018-11-01 18:57:23 +0200

    Add project files.

---
+++ WebApp/Pages/Create.cshtml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace WebApp.Pages
+{
+    public class CreateModel : PageModel
+    {
+        private readonly AppDbContext _db;
+
+        [BindProperty]
+        public Customer Customer { get; set; }
+
+        [TempData]
+        public string Message { get; set; }
+
+        private ILogger<CreateModel> _log;
+
+        public CreateModel(AppDbContext db, ILogger<CreateModel> log)
+        {
+            _db = db;
+            _log = log;
+        }
+
+        public async Task<IActionResult> OnPostAsync()   // tozi method obrabotva metoda post
+        {
+            if (!ModelState.IsValid)         // ako izpratenite danni sa korektni
+            {
+                return Page();
+            }
+            _db.Customers.Add(Customer);     // dobavq customer samo v pametta na aplikaciqta, ako izpratenite danni sa korektni
+
+            await _db.SaveChangesAsync();    // zapisva dannite realno v bazata danni, a ne samo v pametta na aplikaciqta, kakto gorniq red
+
+            var msg = $"Customer { Customer.Name} added."; 
+            Message = msg; 
+            _log.LogCritical(msg);
+
+            return RedirectToPage("/Index");
+        }
+    }
+}
---
+++ WebApp/Pages/Edit.cshtml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApp.Pages
+{
+    public class EditModel : PageModel
+    {
+        private readonly AppDbContext _db;
+
+        public EditModel(AppDbContext db)
+        {
+            _db = db;
+        }
+        [BindProperty]
+        public Customer Customer { get; set; } 
+
+        public async Task<IActionResult> OnGetAsync(int id)
+        {
+            Customer = await _db.Customers.FindAsync(id); // namirame customer v bazata s danni
+
+            if (Customer == null)   // ako customer ne e nameren
+            {
+                return RedirectToPage("/Index"); // vrushta kum nachalnata stranica
+            }
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync() 
+        {
+            if (!ModelState.IsValid) 
+            {
+                return Page();
+            }
+
+            _db.Attach(Customer).State = EntityState.Modified; 
+
+            try  
+            {
+                await _db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException e) // dobavqme e - suobshteneto
+            {
+                throw new Exception($"Customer {Customer.Id} not found!", e); // dobavqme e - suobshteneto
+            }
+
+            return RedirectToAction("./Index");         
+        }
+    }
+}
